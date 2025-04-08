90c75e68e7952f813dde63c08b2431d3edbe4e4e
ivory willows <105597411+undrscre@users.noreply.github.com>
2025-04-09 02:31:05 +0800

    initial projects page

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,7 @@ pub mod routes;
 
 #[tokio::main]
 async fn main() {
-    // let page = routes::other::IndexPage {}.render();
-    // println!("{}", page.unwrap())
-
-    let index = warp::path::end().then(routes::index::page);
-    let about = warp::path("about").then(routes::about::page);
-    let buttons = warp::path("buttons").then(routes::buttons::page);
-    let projects = warp::path("projects").then(routes::projects::page);
-
-    let pages = index.or(about).or(buttons).or(projects);
+    let pages = routes::pages();
     let assets = warp::path("assets").and(warp::fs::dir("assets"));
 
     let routes = assets.or(pages);
--- src/routes/projects.rs
+++ src/routes/projects.rs
@@ -1,10 +1,54 @@
 use askama_warp::Template;
+use serde::Deserialize;
 use warp::reply::Reply;
+use reqwest::header::USER_AGENT;
 
 #[derive(Template)]
 #[template(path="projects.html")]
-pub struct ProjectsPage {}
+pub struct ProjectsPage {
+    repos: Vec<Repository>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Repository {
+    name: String,
+    description: Option<String>,
+    html_url: String,
+    language: Option<String>,
+    stargazers_count: u32,
+}
+
+impl Repository {
+    pub fn description_or_default(&self) -> &str {
+        self.description.as_deref().unwrap_or("No description available")
+    }
+
+    pub fn language_or_default(&self) -> &str {
+        self.language.as_deref().unwrap_or("Unknown")
+    }
+}
+
+pub async fn get_repos() -> Result<Vec<Repository>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let response = client
+        .get("https://api.github.com/users/undrscre/repos")
+        .header(USER_AGENT, "ivrry/1.0")
+        .send()
+        .await?;
+
+    let text = response.text().await?;
+    // println!("Raw response: {}", text);
+
+    let result = serde_json::from_str::<Vec<Repository>>(&text).unwrap();
+    Ok(result)
+}
 
 pub async fn page() -> impl Reply {
-    ProjectsPage {}
+    let repos = get_repos().await.unwrap_or_else(|err| {
+        eprintln!("Error fetching repositories: {:?}", err);
+        vec![] // Return an empty vector if there's an error
+    });
+    ProjectsPage {
+        repos
+    }
 }
