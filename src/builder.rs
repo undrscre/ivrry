5b3bea343a5adfe2684dde71f49f3d0c0e15b79c
ivory willows <105597411+undrscre@users.noreply.github.com>
2025-06-09 02:20:17 +0800

    building??

--- src/builder.rs
+++ src/builder.rs
@@ -1,28 +1,37 @@
 use std::fs;
 
-use minijinja::Environment;
+use minijinja::{Environment, context};
+use crate::contexts::retrieve_context;
 use walkdir::WalkDir;
 
-use crate::contexts::PageContext;
-
-// only used once hopefully,
 pub fn get_environment() -> Environment<'static> {
     let mut env = Environment::new();
     for entry in WalkDir::new("templates").min_depth(1).max_depth(2) {
         let entry = entry.unwrap();
         if entry.file_type().is_file() {
             // what the fuck
-            let name = entry.path().to_str().unwrap().to_owned();
+            let name = entry.path().to_str().unwrap().replace("templates/", "");
             let source = fs::read_to_string(&entry.path()).expect("unable to get source");
             env.add_template_owned(name.clone(), source).expect("unable to create template");
-
-            println!("{}", name);
         }
     }
     env
 }
 
-pub fn generate_page<P: PageContext>(env: &Environment, page: &str) -> String {
+pub fn generate_page(env: &Environment, page: &str) -> Result<String, minijinja::Error> {
     let tmpl = env.get_template(page).expect("unable to get template, is it real?");
-    tmpl.render(P::context()).expect("unable to render template")
+    let ctx = match retrieve_context(page) {
+        Some(p) => p,
+        None => context! {}
+    };
+    Ok(tmpl.render(ctx)?) // ?? what the fuck
+}
+
+pub fn build_all(env: &Environment) -> Result<(), minijinja::Error> {
+    let _ = env.templates().for_each(|t| {
+        println!("generating page {}", t.0);
+        let content = generate_page(env, t.0).expect("unable to compile file");
+        fs::write(format!("{}/{}", "dist", t.0), content).unwrap();
+    });
+    Ok(())
 }
