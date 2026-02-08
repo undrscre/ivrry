632068edc6a65acb3b937a016475aac0aacd8cf7
noelle willows <105597411+undrscre@users.noreply.github.com>
2026-02-08 16:15:02 +0800

    init 2 i think

--- src/contexts/mod.rs
+++ src/contexts/mod.rs
@@ -6,11 +6,6 @@ pub mod diagnostic;
 
 pub async fn retrieve_context(name: &str) -> Option<minijinja::value::Value> {
     match name {
-        "buttons.html" => Some(buttons::context()),
-        "projects.html" => Some(projects::context().await),
-        "about.html" => Some(about::context()),
-        "blog.html" => Some(blog::context()),
-        "diagnostic.html" => Some(diagnostic::context().await),
         _ => None
     }
 }
